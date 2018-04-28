57312a8eb84dc41b3ced324fd575e66e684b14dd
hamstar0 <webutek@gmail.com>
2018-04-28 03:21:22 -0700

    v2.1.0 * Pulled changes from ModConfig branch into master

--- Config.cs
+++ Config.cs
@@ -4,7 +4,6 @@ using Terraria.ModLoader;
 
 
 namespace Injury {
-
 	public class InjuryServerConfigData : ModConfig {
 		public override MultiplayerSyncMode Mode {
 			get { return MultiplayerSyncMode.ServerDictates; }
